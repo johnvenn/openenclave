748901365f3aa238b1d26bf589e24d23ddf7e356
Xiangping Ji <xiangping.ji@intel.com>
2020-09-23 13:28:59 +0000

    add sealed_blob

--- host/sgx/enclave.h
+++ host/sgx/enclave.h
@@ -113,6 +113,9 @@ typedef struct _oe_enclave
     /* Hash of enclave (MRENCLAVE) */
     OE_SHA256 hash;
 
+	/* Sealed Blob for OE PCL */
+	uint8_t* sealed_blob;
+
     /* Array of ocall functions */
     const oe_ocall_func_t* ocalls;
     size_t num_ocalls;
